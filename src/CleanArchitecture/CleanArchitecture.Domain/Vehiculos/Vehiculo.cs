891b0684a0196788d467667e142e6830f6702078
Miguel Angel Ayala Castillo <mayala@ciisa.com>
2026-04-15 22:08:58 -0500

    "feat: archivos iniciales"

---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Vehiculos/Vehiculo.cs
@@ -0,0 +1,20 @@
+using CleanArchitecture.Domain.Abstractions;
+
+namespace CleanArchitecture.Domain.Vehiculos;
+
+public sealed class Vehiculo : Entity
+{
+    public Vehiculo(Guid id) : base(id)
+    {
+        
+    }
+    public Modelo? Modelo {get; private set;}
+    public Vin? Vin {get; private set;}
+    public Direccion? Direccion {get; private set;}
+    public decimal? Precio {get; private set;}
+    public string? TipoMoneda {get; private set;}
+    public decimal? Mantenimiento {get; private set;}
+    public string? MantenimientoTipoMoneda {get; private set;}
+    public DateTime? FechaUltimaActividad {get; private set;}
+    public List <Accesorio> Accesorios {get; private set;} = new ();
+}
