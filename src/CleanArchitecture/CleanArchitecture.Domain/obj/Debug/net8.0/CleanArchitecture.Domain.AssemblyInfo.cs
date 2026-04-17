ddbb7a5a355f0a1a53fd35bb3daa5de6605b511e
Miguel Angel Ayala Castillo <mayala@ciisa.com>
2026-04-16 21:54:53 -0500

    feat: creación de clase usuario con encapsulación

--- src/CleanArchitecture/CleanArchitecture.Domain/obj/Debug/net8.0/CleanArchitecture.Domain.AssemblyInfo.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/obj/Debug/net8.0/CleanArchitecture.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891b0684a0196788d467667e142e6830f6702078")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+834b09942767286c7148d0f5b50e6bb3943ffcd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
