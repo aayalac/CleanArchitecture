2b96e72c23872724dd8b6adb8402e58598dfcb06
Miguel Angel Ayala Castillo <mayala@ciisa.com>
2026-04-28 20:41:55 -0500

    feat: implementación de Estados y Eventos para alquileres

--- src/CleanArchitecture/CleanArchitecture.Domain/Alquileres/AlquilerStatus.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/Alquileres/AlquilerStatus.cs
@@ -3,7 +3,7 @@ namespace CleanArchitecture.Domain.Alquileres;
 public enum AlquilerStatus
 {
     Reservado = 1,
-    confirmado = 2,
+    Confirmado = 2,
     Rechazado = 3,
     Cancelado= 4,
     Completado = 5
