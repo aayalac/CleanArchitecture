2d7a16cc9d18e99d278939827d5c2160ac79f5eb
Miguel Angel Ayala Castillo <ingeniero.ayala@outlook.com>
2026-05-22 20:39:30 -0500

    feat: ajuste unit of work y Domain Events

--- src/CleanArchitecture/CleanArchitecture.Domain/Abstractions/Entity.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/Abstractions/Entity.cs
@@ -15,7 +15,7 @@ public abstract class Entity
         return _domainEvent.ToList();
     }
 
-    public void CrearDomainEvents()
+    public void ClearDomainEvents()
     {
         _domainEvent.Clear();
     }
